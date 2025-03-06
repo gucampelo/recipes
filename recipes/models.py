e7ce51d0835af2669521c7560f919135d8ec378b
gucampe <campelogustavo11@gmail.com>
2025-03-06 01:05:04 -0300

    inicio dos models e configuracao do perfil admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=165)
+
+    def __str__(self):
+        return self.name
+     
+# class Author(models.Model):
+#     name = models.CharField(max_length=165)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=10)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=10)
+    preparation_step = models.TextField()
+    preparation_step_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null = True)
+
+    def __str__(self):
+        return self.title
